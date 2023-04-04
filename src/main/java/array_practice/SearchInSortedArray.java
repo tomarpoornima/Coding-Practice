032162ca2387a08ec83de26978a7715503ef861f
Poornima Tomar <tomarpoornima52@gmail.com>
2023-04-04 18:19:36 +0530

    added updated code file in array_practice

--- src/main/java/array_practice/SearchInSortedArray.java
+++ src/main/java/array_practice/SearchInSortedArray.java
@@ -13,7 +13,7 @@ public class SearchInSortedArray {
         }
         return -1;
     }
-
+    //TC - O(logn) sc - O(1)
     public static int binarySearch(int[] nums, int target){
         int l =0, r = nums.length-1;
 
@@ -39,6 +39,33 @@ public class SearchInSortedArray {
         }
         return -1;
     }
+    public static int binarySearchDuplicate(int[] nums, int key){
+        int l=0, r=nums.length-1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == key){
+                return mid;
+            }
+            if(nums[mid] == nums[l] && nums[mid] == nums[r]){
+                l++;
+                r--;
+            }
+            if(nums[l]<= nums[mid]){
+                if(key < nums[mid] && key >= nums[l]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(key > nums[mid] && key <= nums[r]){
+                    l = mid + 1;
+                }else{
+                    r = mid - 1;
+                }
+            }
+        }
+        return  -1;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -50,5 +77,6 @@ public class SearchInSortedArray {
         int targte = sc.nextInt();
         System.out.println(search(nums, targte));
         System.out.println(binarySearch(nums, targte));
+        System.out.println(binarySearchDuplicate(nums, targte));
     }
 }
