1a950e9085ccde85bf0cce53de6d86ff5ead3e17
tomarpoornima <tomarpoornima52@gmail.com>
2023-04-11 11:58:16 +0530

    updated

--- src/main/java/string_package/RemoveStarsFromString.java
+++ src/main/java/string_package/RemoveStarsFromString.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class RemoveStarsFromString {
 
+    //TC - O(n), sc - O(n) using string
     public static String removeStars(String s){
         StringBuilder  ans = new StringBuilder();
         for(int i=0; i<s.length(); i++){
@@ -16,10 +17,31 @@ public class RemoveStarsFromString {
         return ans.toString();
     }
 
+    //using two pointer tc- O(n), sc - O(n)
+    public static String removeStars1(String s){
+        char[] chars = new char[s.length()];
+        int j=0;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (c == '*') {
+                j--;
+            }else{
+                chars[j++] = c;
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for(int i=0; i<j; i++){
+            ans.append(chars[i]);
+        }
+        return ans.toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         System.out.println(removeStars(s));
+        System.out.println(removeStars1(s));
 
     }
 }
