d2b3d07a9590fc1aa3256db558d860b1f9048f0c
tomarpoornima <tomarpoornima52@gmail.com>
2023-04-11 12:18:49 +0530

    updated

--- src/main/java/string_package/RemoveStarsFromString.java
+++ src/main/java/string_package/RemoveStarsFromString.java
@@ -1,6 +1,7 @@
 package string_package;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class RemoveStarsFromString {
 
@@ -37,11 +38,29 @@ public class RemoveStarsFromString {
         return ans.toString();
     }
 
+    //using stack tc- O(n), sc -O(n)
+    public static String removeStars2(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '*'){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while (!st.isEmpty()){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         System.out.println(removeStars(s));
         System.out.println(removeStars1(s));
+        System.out.println(removeStars2(s));
 
     }
 }
